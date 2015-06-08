ca24e071eaebb3383cb8bdbfbc64954b650ac241
Brandon Courtney <brandon.h.courtney@gmail.com>
2015-06-08 17:36:08 -0500

    Aesthetic Cleanup

--- sumArray.js
+++ sumArray.js
@@ -8,12 +8,14 @@
 // 3.) Call the function sumArray with the sumArray as an argument.
 // e.g. - sumArray(sumNumbers);
 
-var sumNumbers = [];
+var sumNumbers = [];							
 var sumArray = function(array) {
-var length = sumNumbers.length;
-var sumTotal = 0;
-	for( var i = 0; i < length; i++ ) {
-		sumTotal += array[i];
-	}
+	var length = sumNumbers.length;
+	var sumTotal = 0;
+		for( var i = 0; i < length; i++ ) {
+			sumTotal += array[i];
+		}
 	return sumTotal;
 }
+												// e.g. -  sumNumbers = [1, 2, 3];
+												// e.g. -  sumArray(sumNumbers);
