574cbd9521865e1f4cf93199a93724f5fa424a43
Brandon Courtney <brandon.h.courtney@gmail.com>
2015-06-08 16:17:29 -0500

    first commit

--- sumArray.js
+++ sumArray.js
@@ -1,14 +1,19 @@
 // When an array of numbers is passed to this 
-// function it sums the numbers.
+// function it sums the numbers in the array.
+
+// Instructions: 
+// 1.) Copy/Paste code below into the console.
+// 2.) Set the array sumNumbers equal to an array of numbers.
+// e.g. -  sumNumbers = [1, 2, 3];
+// 3.) Call the function sumArray with the sumArray as an argument.
+// e.g. - sumArray(sumNumbers);
 
 var sumNumbers = [];
-var length = sumNumbers.length;
 var sumArray = function(array) {
-	var sumTotal = 0;
-		for( var i = 0; i < length; i++ ) {
-			sumTotal += array[i];
-		}
-		return sumTotal;
+var length = sumNumbers.length;
+var sumTotal = 0;
+	for( var i = 0; i < length; i++ ) {
+		sumTotal += array[i];
+	}
+	return sumTotal;
 }
-
-console.log(sumArray(sumNumbers));
